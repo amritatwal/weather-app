01798776986198b5ee844787c9a5726d306aa1cd
adam960 <91432921+ah960@users.noreply.github.com>
2022-05-05 11:42:13 +0100

    carousel update 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,13 @@ function DailyCard({ location, apiResponse, date }) {
   return (
     <Card hoverable clickable onClick={() => console.log("ive been clicked")}>
       <Card.Body css={{ p: 0 }}>
-        <Text b>{date}</Text>
+        <Text h4>{date}</Text>
         <Row>
-          <Text h2>{`${Math.round(apiResponse.temp.day)}°C`}</Text>
+          <Text h1>{`${Math.round(apiResponse.temp.day)}°`}</Text>
           <Card.Image
             objectFit="cover"
             src={`https://openweathermap.org/img/wn/${apiResponse.weather[0].icon}@4x.png`}
-            width="66%"
+            width="%"
             height={140}
             alt={date}
           />
